ec27b6039fbbc1e4f006c81168a6e8b2696fb3fb
avulaankith <avulaankith@gmail.com>
2025-02-07 20:06:14 -0600

    adding aws and favicon changes

--- src/components/SkillsData.jsx
+++ src/components/SkillsData.jsx
@@ -2,21 +2,49 @@
 const skills = [
   [
     { name: "Python", imageSrc: "images/python-logo.svg", altText: "Python" },
-    { name: "PyTorch", imageSrc: "images/pytorch-logo.svg", altText: "PyTorch" },
-    { name: "Tensorflow", imageSrc: "images/tensorflow-logo.svg", altText: "Tensorflow" },
+    {
+      name: "PyTorch",
+      imageSrc: "images/pytorch-logo.svg",
+      altText: "PyTorch",
+    },
+    {
+      name: "Tensorflow",
+      imageSrc: "images/tensorflow-logo.svg",
+      altText: "Tensorflow",
+    },
     { name: "MySQL", imageSrc: "images/mysql-logo.svg", altText: "MySQL" },
-    { name: "Apache Spark", imageSrc: "images/spark-logo.svg", altText: "Apache Spark" },
+    {
+      name: "Apache Spark",
+      imageSrc: "images/spark-logo.svg",
+      altText: "Apache Spark",
+    },
     { name: "Hadoop", imageSrc: "images/hadoop-logo.svg", altText: "Hadoop" },
     // ... more skills for the first row ...
   ],
   [
     { name: "HTML", imageSrc: "images/html-logo.svg", altText: "HTML Logo" },
     { name: "CSS", imageSrc: "images/css-logo.svg", altText: "CSS Logo" },
-    { name: "JavaScript", imageSrc: "images/javascript-logo.svg", altText: "JS Logo" },
+    {
+      name: "JavaScript",
+      imageSrc: "images/javascript-logo.svg",
+      altText: "JS Logo",
+    },
     { name: "React", imageSrc: "images/react-logo.svg", altText: "React Logo" },
     { name: "Flask", imageSrc: "images/flask-logo.svg", altText: "Flask Logo" },
     { name: "Git", imageSrc: "images/github-logo.svg", altText: "Git Logo" },
     // ... more skills for the second row ...
+  ],
+  [
+    {
+      name: "AWS",
+      imageSrc: "images/aws-svgrepo-com.svg",
+      altText: "HTML Logo",
+    },
+    {
+      name: "Docker",
+      imageSrc: "images/docker-svgrepo-com.svg",
+      altText: "Docker Logo",
+    },
   ],
   // ... more rows as needed ...
 ];
